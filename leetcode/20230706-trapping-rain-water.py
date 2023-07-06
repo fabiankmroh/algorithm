24d63f941f320640864f697948cadfd52f065863
Fabian Roh <fabiankmroh@gmail.com>
2023-07-06 23:05:58 +0900

    Rain Water: In Progress

--- leetcode/20230706-trapping-rain-water.py
+++ leetcode/20230706-trapping-rain-water.py
@@ -3,3 +3,7 @@ class Solution:
     def trap(self, height: List[int]) -> int:
         for i in height:
             self.wall_stack.append(i)
+            size = len(self.wall_stack)
+            
+            if i > wall_stack[size]:
+                
